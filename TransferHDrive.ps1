aa79f733b76b6e78531c903a6de42018c670e1b4
BlankTheGamerGod <152419165+BlankTheGamerGod@users.noreply.github.com>
2023-11-30 14:33:28 -0600

    Update TransferHDrive.ps1

--- TransferHDrive.ps1
+++ TransferHDrive.ps1
@@ -1,10 +1,49 @@
-﻿Start-Transcript -Path "C:\temp\TransferTranscript.txt"
+# NOTE: New stuff is marked with comments that start with "new - "
+
+Start-Transcript -Path "C:\temp\TransferTranscript.txt"
 import-module ActiveDirectory
 
 # function to get paths over 250
 Function filesOverLength ($homeDirectory) {
     Dir -LiteralPath ('\\?\UNC\' + $homeDirectory.substring(2)) -Recurse | Select-Object -Property FullName, @{Name="FullNameLength";Expression={($_.FullName.Length)}} | Where-Object {$_.FullName.length -ge 250}
 }      
+
+# new - function to check if an item is offending the naming guidelines
+function itemsToBeSkipped ($items) {
+    # an array to collect the files to not copy over
+    $forbiddenItems = @()
+
+    # forbidden char string
+    $forbiddenChars = '"*:<>?/\|~#%&{}$'
+
+    # forbidden name array
+    $forbiddenNames = @('.lock', 'CON', 'PRN', 'AUX', 'NUL', 'COM1', 'COM2', 'COM3', 'COM4', 'COM5', 'COM6', 'COM7', 'COM8', 'COM9', 'LPT1', 'LPT2', 'LPT3', 'LPT4', 'LPT5', 'LPT6', 'LPT7', 'LPT8', 'LPT9', 'desktop.in')
+
+    # tried to do regex matching, not sure if it's correct
+    # regex escape is used because the special characters need to be treated as literal characters in this context
+    # wildcard matching used in the else if (example $item.Name -like "*.one")
+    # character class is used to find files with ANY character from the forbidden char set
+    foreach($item in $items) {
+        # https://stackoverflow.com/questions/39825440/check-if-a-path-is-a-folder-or-a-file-in-powershell
+        $isFolder = Test-Path -Path $item.FullName -PathType Container
+
+        if ($isFolder -and $item.Name -eq "forms") {
+            $forbiddenItems += $item.FullName
+        } elseif (-not $isFolder -and ($item.Name -match "[$([regex]::Escape($forbiddenChars))]") 
+                    -or $item.Name -like "*.vti*" 
+                    -or $item.Name -like "~*"
+                    -or $item.Name -like "*.one") {
+            $forbiddenItems += $item.FullName
+        }
+
+        # check BOTH folders AND files
+        if ($forbiddenNames -contains $item.Name) {
+            $forbiddenItems += $item.FullName
+        }
+    }
+ 
+    return $forbiddenItems
+}
 
 #Grab UserName
 [string]$domainUser = Get-WmiObject -class Win32_computersystem | Select -ExpandProperty username
@@ -29,8 +68,9 @@ Try {
     $filesToSkip = filesOverLength -homeDirectory $homeDirectory
 
     # output skipped files to user
+    # new - changed output message to be more clear since we now need to skip many different types of files/folders
     if ($filesToSkip.count -gt 0) {
-        Write-Output ("Skipped Files:")
+        Write-Output ("Files skipped because their name was too long:")
 
         # Iterate through the list and output each object
         $filesToSkip | ForEach-Object {
@@ -38,11 +78,31 @@ Try {
         }
     }
 
-    # copy items excluding the ones in the array we just got
-    Get-ChildItem -LiteralPath ('\\?\UNC\' + $homeDirectory.substring(2)) -Recurse | Where-Object {$_.FullName -notin $filesToSkip.FullName} | ForEach-Object {
-        $relativePath = $_.FullName.Substring(('\\?\UNC\' + $homeDirectory.substring(2)).Length + 1)
+    # new - get all files to be processed before copying
+    $allItems = Get-ChildItem -LiteralPath ('\\?\UNC\' + $homeDirectory.substring(2)) -Recurse
+
+    # new - call the function to filter out forbidden chars/phrases
+    $forbiddenItems = itemsToBeSkipped $allItems
+
+    # new - output skipped files to user
+    if ($forbiddenItems.count -gt 0) {
+        # note - it shows `" because it is needed to print " in a string
+        Write-Output ("Items skipped because they violate naming conventions or have a `".one`" extension")
+
+        # Iterate through the list and output each object
+        $forbiddenItems | ForEach-Object {
+            Write-Output $_
+        }
+    }
+
+    # new - copy items excluding the ones in the array we just got
+    $filesToCopy = $allFiles | Where-Object { $_.FullName -notin $filesToSkip.FullName -and $_.Name -notin $forbiddenItems }
+
+    # new - it is now in a foreach loop
+    foreach ($file in $filesToCopy) {
+        $relativePath = $file.FullName.Substring(('\\?\UNC\' + $homeDirectory.substring(2)).Length + 1)
         $destinationPath = Join-Path -Path "C:\Users\$user\OneDrive - Xcel Energy Services Inc\H Drive" -ChildPath $relativePath
-        Copy-Item -LiteralPath $_.FullName -Destination $destinationPath -Recurse -Force
+        Copy-Item -LiteralPath $file.FullName -Destination $destinationPath -Recurse -Force
     }
 } Catch {
     $_.Exception.Message | Out-File -FilePath "C:\temp\errorLog.txt"  
@@ -72,4 +132,3 @@ Try {
          Stop-Transcript
     }
 }
-            
