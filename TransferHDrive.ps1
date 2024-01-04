f06b08add4359f83b21c0d890d5350ca081f0424
BlankTheGamerGod <152419165+BlankTheGamerGod@users.noreply.github.com>
2024-01-04 13:01:40 -0600

    Update TransferHDrive.ps1

--- TransferHDrive.ps1
+++ TransferHDrive.ps1
@@ -49,9 +49,9 @@ Try {
 #Verification through Hash
 } finally {                
     Get-ChildItem -Path ('\\?\UNC\' + $homeDirectory.substring(2)) -Recurse -Exclude *.one, *.onetoc2, *.onepkg |
-    Get-FileHash -Algorithm SHA256 | Select-Object -Property Hash | Out-File "C:\temp\HDriveHash.txt"
+    Get-FileHash -Algorithm SHA256 | Select-Object -Property Hash | Out-File "C:\temp\MitigationLogs\HDriveHash.txt"
     Get-Childitem -Path "C:\Users\$user\OneDrive - Xcel Energy Services Inc\H Drive.$env:computername" -Recurse |
-    Get-FileHash -Algorithm SHA256 | Select-Object -Property Hash | Out-File "C:\temp\OneDriveHash.txt"
+    Get-FileHash -Algorithm SHA256 | Select-Object -Property Hash | Out-File "C:\temp\MitigationLogs\OneDriveHash.txt"
 
     $objects =@{
         ReferenceObject = (Get-Content -Path "C:\temp\MitigationLogs\HDriveHash.txt")
